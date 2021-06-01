db558719f65b23c4dcc9b5363bb608f20e08538a
hammyster <69561461+hammyster@users.noreply.github.com>
2021-06-01 04:18:17 -0300

    Adding files

---
+++ src/AP/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace AP
+{
+    partial class AP
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "AutoPotion - OFF";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ON (F2) - OFF (F3)";
+            // 
+            // AP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(194, 51);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "AP";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AP";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.timer1_Tick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button Active;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ src/AP/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace AP
+{
+    public partial class AP : Form
+    {
+        [DllImport("user32.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Unicode, EntryPoint = "keybd_event", ExactSpelling = true, SetLastError = true)]
+        public static extern void KEYB_Event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+
+        [DllImport("user32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        private static extern Keys GetAsyncKeyState(Keys Tecla);
+
+        [DllImport("user32", CharSet = CharSet.Ansi, EntryPoint = "GetAsyncKeyState", ExactSpelling = true, SetLastError = true)]
+        private static extern int GetKeyPress(int key);
+        public AP()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (GetKeyPress(113) != 0)
+            {
+                this.label1.Text = "AutoPotion - ON";
+                this.timer2.Start();
+            }
+            else if (GetKeyPress(114) != 0)
+            {
+                this.label1.Text = "AutoPotion - OFF";
+                this.timer2.Stop();
+            }
+
+            if (GetKeyPress(163) != 0)
+            {
+                this.Opacity = 100;
+                this.ShowInTaskbar = true;
+            }
+            else if (GetKeyPress(161) != 0)
+            {
+                this.Opacity = 0;
+                this.ShowInTaskbar = false;
+            }
+
+        }
+
+        private const byte VK_CONTROL = 17;
+        private const byte VK_KEY_Q = 81;
+        private const byte VK_KEY_W = 87;
+        private const byte VK_KEY_E = 69;
+        private const byte KEYEVENTF_KEYUP = 2;
+        private const byte VK_KEY_1 = 49;
+        private const byte VK_KEY_2 = 50;
+        private const byte VK_KEY_3 = 51;
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            AP.KEYB_Event(81, 0, 0, 0);
+            AP.KEYB_Event(81, 0, 2, 0);
+            AP.KEYB_Event(87, 0, 0, 0);
+            AP.KEYB_Event(87, 0, 2, 0);
+            AP.KEYB_Event(69, 0, 0, 0);
+            AP.KEYB_Event(69, 0, 2, 0);
+
+            Thread.Sleep(70);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.timer1.Start();
+            this.label1.Text = "AutoPotion - ON";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.timer1.Stop();
+            this.label1.Text = "AutoPotion - OFF";
+        }
+    }
+}
